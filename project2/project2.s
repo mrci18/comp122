0e8e383d17204a1ba3632833184bdeb04d8819a2
mrci18 <charles.ibo.247@my.csun.edu>
2017-11-07 00:52:18 -0800

    Missing character output message

--- project2/project2.s
+++ project2/project2.s
@@ -1,9 +1,15 @@
+;Charles Ibo
+;Comp 122/L
+;Project 2
+
+;Open File
 mov r1,#0
 ldr r0,=StringFile
 
 swi 0x66	;open file
-bcs Whoops
+bcs ExitFile
 
+;Store file handle
 mov r3,r0 	;store file handle in r3
 mov r0,r3	;move file handle into r0
 
@@ -11,12 +17,12 @@ mov r0,r3	;move file handle into r0
 ldr r1,=InputString
 mov r2,#256
 swi 0x6a 	;read string
-bcs Whoops
 
 ;Check if input.txt has characters
-;cmp r0,#0	;No characters read means EOF
-;bcs Whoops
+cmp r0,#0	;No characters read means EOF
+beq ExitNoChar
 
+;Loop to see if input.txt has a lowercase, uppercase, forward slash, space, apostraphe, and comma
 charloop:
 ldrb r2, [r1]
 cmp  r2, #0
@@ -37,19 +43,16 @@ cmp  r2, #39
 beq  copy
 cmp  r2, #44
 beq  copy
-;cmp  r2, #' '
-;beq  copy
-;bal  skip
 
+;Check if there has been two consecutive forward slashes
 count:
 add  r6,r2,r6
 cmp  r6, #94
 beq  double
 cmp  r6, #47
 beq  skip
-;add  r0, r0, #1
-;bal charloop
 
+;Remove first slash and indent to a new line
 double:
 mov  r2,#32
 strb r2, [r1], #1
@@ -59,34 +62,49 @@ mov  r6,#0
 cmp  r6,#0
 beq  skip
 
+;Subtract 32 and make letter uppercase
 lower:
 sub  r2, r2, #32
 
+;Place in output.txt
 copy:
 strb r2, [r1], #1
 
+;Check next position in input.txt
 skip:
 add  r0, r0, #1
 bal  charloop
 
-;ldrb r0,[r1]
-;cmp r0,#80 ; Check for 'P' with decimal
-;cmp r0,#0x50; Check for 'P' with hexadecimal
-;cmp r0,#'P; Check for 'P' with character
-
+;Print what we gathered
 endofstring: 
 mov r1,#1
 ldr r0,=OutputFile
 
 swi 0x66	;Open output file
-bcs Whoops
+bcs ExitFinal
 
 ldr r1,=InputString
 
 swi 0x69	;print string to file
-bcs Whoops
+bcs ExitFinal
 
-Whoops:
+;Message saying no characters in file:
+ExitNoChar:
+ldr r0,=exitNoChar
+swi 0x02
+exitNoChar: .asciz "There are no characters in input.txt"
+B ExitFinal
+
+;Message saying there was no file found:
+ExitFile:
+ldr r0,=MyString
+swi 0x02
+MyString: .asciz "File not found, please create file before continuing"
+
+ExitFinal:
+ldr r0,=Output
+swi 0x02
+Output: .asciz "Your string has been printed in an output.txt file"
 swi 0x11	;Exit
 
 
