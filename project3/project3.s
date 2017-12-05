c40ada38eea39b49329213d5b1412bf2d8e3c10f
mrci18 <charles.ibo.247@my.csun.edu>
2017-12-05 01:20:17 -0800

    Fixed add and subtract based on blue button pressed

--- project3/project3.s
+++ project3/project3.s
@@ -1,8 +1,41 @@
+;Charles Ibo
+;Project 3
+
+;Display 1000 on LED screen
 mov r0,#1
 mov r1,#1
 mov r2,#1000
 swi 0x205 ;Display integer
 BCS End
+
+;Tell user to press black button 
+PressBlack:
+ldr r0,=PressBLK
+swi 0x02
+PressBLK: .asciz "Press one black button\n"
+
+;Check whether user pressed left or right
+Black:
+swi 0x202
+cmp r0,#0x02
+BEQ Left
+BAL Right
+
+;Turn on left light
+Left:
+mov r0,#0x02
+swi 0x201 ;Left LED on
+mov r3,#2
+BAL PressBlue
+
+;Turn on right light
+Right:
+cmp r0,#0
+BEQ NotPressed
+mov r0,#0x01
+swi 0x201 ;Right LED on
+mov r3,#1
+BAL PressBlue
 
 ;Tell user to press blue button
 PressBlue:
@@ -15,36 +48,52 @@ Blue:
 swi 0x203
 cmp r0,#1
 BEQ Zero
+
 cmp r0,#2
 BEQ One
+
 cmp r0,#4
 BEQ Two
+
 cmp r0,#8
 BEQ Three
+
 cmp r0,#16
 BEQ Four
+
 cmp r0,#32
 BEQ Five
+
 cmp r0,#64
 BEQ Six
+
 cmp r0,#128
 BEQ Seven
+
 cmp r0,#256
 BEQ Eight
+
 cmp r0,#512
 BEQ Nine
+
 cmp r0,#1024
 BEQ A
+
 cmp r0,#2048
 BEQ B
+
 cmp r0,#4096
 BEQ C
+
 cmp r0,#8192
 BEQ D
+
 cmp r0,#16384
 BEQ E
+
 cmp r0,#32768
 BEQ F
+
 cmp r0,#0
 BEQ NotPressed
 
@@ -82,86 +131,100 @@ BAL PressBlack
 Zero:
 mov r0,#0xED
 swi 0x200
-BAL PressBlack
+mov r7,#0
+cmp r3,#2
+BEQ Subtract
+BAL Add
 
 One:
 mov r0,#0x60
 swi 0x200
-BAL PressBlack
+mov r7,#1
+cmp r3,#2
+BEQ Subtract
+BAL Add
 
 Two:
 mov r0,#0xCE
 swi 0x200
-BAL PressBlack
+mov r7,#2
+cmp r3,#2
+BEQ Subtract
+BAL Add
 
 Three:
 mov r0,#0xEA
 swi 0x200
-BAL PressBlack
+mov r7,#3
+cmp r3,#2
+BEQ Subtract
+BAL Add
 
 Four:
 mov r0,#0x63
 swi 0x200
-BAL PressBlack
+mov r7,#4
+cmp r3,#2
+BEQ Subtract
+BAL Add
 
 Five:
 mov r0,#0xAB
 swi 0x200
-BAL PressBlack
+mov r7,#5
+cmp r3,#2
+BEQ Subtract
+BAL Add
 
 Six:
 mov r0,#0xAF
 swi 0x200
-BAL PressBlack
+mov r7,#6
+cmp r3,#2
+BEQ Subtract
+BAL Add
 
 Seven:
 mov r0,#0xE0
 swi 0x200
-BAL PressBlack
+mov r7,#7
+cmp r3,#2
+BEQ Subtract
+BAL Add
 
 Eight:
 mov r0,#0xEF
 swi 0x200
-BAL PressBlack
+mov r7,#8
+cmp r3,#2
+BEQ Subtract
+BAL Add
 
 Nine:
 mov r0,#0xE3
 swi 0x200
-BAL PressBlack
-
-;Tell user to press black button 
-PressBlack:
-ldr r0,=PressBLK
-swi 0x02
-PressBLK: .asciz "Press one black button\n"
-
-Black:
-swi 0x202
-cmp r0,#0x02
-BEQ Left
-BAL Right
-
-Left:
-mov r0,#0x02
-swi 0x201 ;Left LED on
-BAL Subtract
-
-Right:
-cmp r0,#0
-BEQ NotPressed
-mov r0,#0x01
-swi 0x201 ;Right LED on
+mov r7,#9
+cmp r3,#2
+BEQ Subtract
 BAL Add
 
+;Subtract value from 8 segment display if digit and display on LED
 Subtract:
-SUB r2,r2,#1
+mov r0,#1
+mov r1,#1
+SUB r2,r2,r7
+swi 0x206
 swi 0x205
-BAL PressBlue
+BAL PressBlack
 
+;Add value from 8 segment display if digit and display on LED
 Add:
-ADD r2,r2,#1
+mov r0,#1
+mov r1,#1
+ADD r2,r2,r7
+swi 0x206
 swi 0x205
-BAL PressBlue
+BAL PressBlack
 
 ;Tell user that they did not press a button 
 NotPressed:
